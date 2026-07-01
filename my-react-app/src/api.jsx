ec79eb85beac717bc60a3336da2b1bc7c4ab4fca
charan-2007-26 <charan887795@gmail.com>
2026-07-01 14:27:20 +0530

    CRUD Operations

--- my-react-app/src/api.jsx
+++ my-react-app/src/api.jsx
@@ -1,68 +1,99 @@
-import {useState , useEffect} from "react";
+import React,{useState , useEffect} from "react";
 import axios from "axios";
-function API()
+function crud ()
 {
     const [user , setuser]=useState([]);
-    const[loading ,setloading]=useState(true);
-    const [error , seterror]=useState("");
-    useEffect(
-        ()=>{
-            axios
-            .get("https://jsonplaceholder.typicode.com/users")
-            .then((Response)=>{
-                setuser(Response.data);
-                setloading(false);
-            })
-            .catch((err)=>
-            {
-                seterror("Failure");
-                setloading(false);
-            }
-            );
-        },[]
-    );
-    if(loading)
+    const[name , setname]= useState("");
+    const getUser=()=>{
+        axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((Response)=>{
+            setuser(Response.data);
+        })
+        .catch((err)=>{
+            console.log("Failed");
+        });
+    };
+    useEffect(()=>{
+        getUser();
+    },[]);
+    const adduser =()=>
     {
-        return (
-            <h2>Loading....</h2>
-        );
-    }
-    if (error)
+        const NewUser = {
+            name:name,
+        };
+        axios
+        .post("https://jsonplaceholder.typicode.com/users",NewUser)
+        .then((Response)=>{
+            alert("User Added");
+            console.log(Response.data);
+            setuser([...user,Response.data]);
+            setname("");
+        })
+        .catch((err)=>
+        {
+            console.log("Error");
+        });
+    };
+    const updateuser =(id)=>
     {
-        return (
-            <h2>Error....</h2>
-        );
-    }
+        axios
+        .put(`https://jsonplaceholder.typicode.com/users/${id}`,{
+            name:"Charan",
+        })
+        .then((Response)=>{
+            alert("User Updated");
+            console.log("Updated");
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+    };
+    const deluser=(id)=>{
+        axios
+        .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(()=>{
+            alert("User Deleted");
+        });
+    };
+
     return (
         <>
-        <div className="container">
-            <h1>User Details</h1>
+        <div style={{textAlign:"center"}}>
+            <h1>CRUD Operations in Axios</h1>
+            <br />
+            <label>Name:</label>
+            <input type="text" placeholder="Vishnu" 
+            value={name}
+            onChange={(e)=>setname(e.target.value)}/>
+            <br />
+            <button onClick={adduser} >Submit</button>
+            <br />
             <table border={"1"}>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Address</th>
-                    <th>Company</th>
-                </tr>
-                    {user.map((user)=>(
-                        <tr key={user.id}>
-                        <td>{user.id}</td>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.phone}</td>
-                        <td>{user.address.street}, {user.address.suite}, {user.address.city}</td>
-                        <td>{user.company.name}</td>
-                        </tr>
-                    ))
-                    
-                    }
-                
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+        </tr>
+        {user.map((user)=>{
+            <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                    <button onClick={()=>updateuser(user.id)}>
+                    Update
+                    </button>
+                    <button onClick={()=>deluser(user.id)}>
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        })}
             </table>
         </div>
         </>
     );
-
 }
-export default API;
+export default crud;
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import JSX from "./api.jsx"
+import CRUD from "./CRUD.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <JSX></JSX>
+  <CRUD />
   </StrictMode>,
 )
